326dab2ab8273cb3a096007e999e4629987e9013
Łukasz Jan Niemier <lukasz@niemier.pl>
2016-04-01 01:43:35 +0200

    Add lnav

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,7 @@ node default {
       key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJvEGHPG3UUkTNszvyciolaCTJYrvFYNMlrJuyGw8V6c7gtKBSUj3tZwV52ykxAGwZlecACs8CgttUT/ERL2BiOMtNRDje8t3Y2h7OBLYIxKP3LI43n4luidS7Fr05cyvIp5/fF2HwSet4/nF6qWL5ij+fvE+9l3XBjhvYl3nJjdYRXkJUsIo5T4j5Gu5ssdjzliWbs7cK8odl2wCncsI5SNaD7G9BGpRnGrg/ZeQ/1ZZD65M/aniz2oHbHA0Cm573hGkPcidkMNfSQRKvpyvwj8lxFRB4kgHL8WEKxKqmqi7Su5WRrMCKbnccB7r4C3obqO1OaCfiuevDrc3etWBl';
   }
 
-  package { ['tmux', 'iptables', 'mosh', 'postfix']: }
+  package { ['tmux', 'iptables', 'mosh', 'postfix', 'lnav']: }
 
   service {
     'ssh':
