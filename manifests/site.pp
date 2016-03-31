5702d36c2c73355313e8c9474790141191490022
Łukasz Jan Niemier <lukasz@niemier.pl>
2016-04-01 01:41:31 +0200

    Fix SSH keys entry

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,7 +17,7 @@ node default {
       ensure => present,
   }
 
-  ssh_authorized_keys {
+  ssh_authorized_key {
     'hauleth@athena':
       ensure => present,
       user   => 'hauleth',
@@ -25,9 +25,7 @@ node default {
       key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJvEGHPG3UUkTNszvyciolaCTJYrvFYNMlrJuyGw8V6c7gtKBSUj3tZwV52ykxAGwZlecACs8CgttUT/ERL2BiOMtNRDje8t3Y2h7OBLYIxKP3LI43n4luidS7Fr05cyvIp5/fF2HwSet4/nF6qWL5ij+fvE+9l3XBjhvYl3nJjdYRXkJUsIo5T4j5Gu5ssdjzliWbs7cK8odl2wCncsI5SNaD7G9BGpRnGrg/ZeQ/1ZZD65M/aniz2oHbHA0Cm573hGkPcidkMNfSQRKvpyvwj8lxFRB4kgHL8WEKxKqmqi7Su5WRrMCKbnccB7r4C3obqO1OaCfiuevDrc3etWBl';
   }
 
-  package {
-    ['tmux', 'iptables', 'mosh']:
-  }
+  package { ['tmux', 'iptables', 'mosh', 'postfix']: }
 
   service {
     'ssh':
