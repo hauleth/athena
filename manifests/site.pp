bbc86865be9624f4104f7529baa4a8edaafe1b9e
Łukasz Jan Niemier <lukasz@niemier.pl>
2016-04-01 00:26:23 +0200

    Install some useful packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,12 +25,8 @@ node default {
       key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJvEGHPG3UUkTNszvyciolaCTJYrvFYNMlrJuyGw8V6c7gtKBSUj3tZwV52ykxAGwZlecACs8CgttUT/ERL2BiOMtNRDje8t3Y2h7OBLYIxKP3LI43n4luidS7Fr05cyvIp5/fF2HwSet4/nF6qWL5ij+fvE+9l3XBjhvYl3nJjdYRXkJUsIo5T4j5Gu5ssdjzliWbs7cK8odl2wCncsI5SNaD7G9BGpRnGrg/ZeQ/1ZZD65M/aniz2oHbHA0Cm573hGkPcidkMNfSQRKvpyvwj8lxFRB4kgHL8WEKxKqmqi7Su5WRrMCKbnccB7r4C3obqO1OaCfiuevDrc3etWBl';
   }
 
-  class { 'nginx':
-    manage_repo => true,
-  }
-
-  class { 'letsencrypt':
-    email =>  'lukasz@niemier.pl',
+  package {
+    ['tmux', 'iptables', 'mosh']:
   }
 
   service {
@@ -40,6 +36,14 @@ node default {
     'puppet':
       ensure => stopped,
       enable => false;
+  }
+
+  class { 'nginx':
+    manage_repo => true,
+  }
+
+  class { 'letsencrypt':
+    email =>  'lukasz@niemier.pl',
   }
 
   nginx::resource::vhost {
