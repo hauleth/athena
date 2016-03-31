2877999b71f27c595727e3b3648cade07760f254
Łukasz Jan Niemier <lukasz@niemier.pl>
2016-03-31 23:39:09 +0200

    Refactor manifests

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,28 @@
 node default {
+  User {
+    shell      => '/bin/bash',
+    managehome => true,
+  }
+  Package {
+    ensure => latest,
+  }
+
   include cron-puppet
 
-  user { 'hauleth':
-    ensure     => present,
-    groups     => ['sudo'],
-    managehome => true,
-    shell      => '/bin/bash',
+  user {
+    'hauleth':
+      ensure     => present,
+      groups     => ['sudo'];
+    'pyskata':
+      ensure => present,
+  }
+
+  ssh_authorized_keys {
+    'hauleth@athena':
+      ensure => present,
+      user   => 'hauleth',
+      type   => 'ssh-rsa',
+      key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJvEGHPG3UUkTNszvyciolaCTJYrvFYNMlrJuyGw8V6c7gtKBSUj3tZwV52ykxAGwZlecACs8CgttUT/ERL2BiOMtNRDje8t3Y2h7OBLYIxKP3LI43n4luidS7Fr05cyvIp5/fF2HwSet4/nF6qWL5ij+fvE+9l3XBjhvYl3nJjdYRXkJUsIo5T4j5Gu5ssdjzliWbs7cK8odl2wCncsI5SNaD7G9BGpRnGrg/ZeQ/1ZZD65M/aniz2oHbHA0Cm573hGkPcidkMNfSQRKvpyvwj8lxFRB4kgHL8WEKxKqmqi7Su5WRrMCKbnccB7r4C3obqO1OaCfiuevDrc3etWBl';
   }
 
   class { 'nginx':
@@ -16,15 +33,26 @@ node default {
     email =>  'lukasz@niemier.pl',
   }
 
-  file { '/var/www':
-    ensure => directory,
+  nginx::resource::vhost {
+    'athena.niemier.pl':
+      ensure   => absent,
+      www_root => '/var/www',
+      require  => File['/var/www'];
+    'matuszewska.photo':
+      ensure           => present,
+      www_root         => '/home/pyskata/www',
+      ssl              => true,
+      ssl_cert         => '/etc/letsencrypt/live/matuszewska.photo/fullchain.pem',
+      ssl_key          => '/etc/letsencrypt/live/matuszewska.photo/privkey.pem',
+      rewrite_to_https => true,
+      require          => User['pyskata'],
   }
 
-  nginx::resource::vhost { 'athena.niemier.pl':
-    ensure   => present,
-    www_root => '/var/www',
-    require  => File['/var/www'],
+  letsencrypt::certonly {
+    'matuszewska.photo':
+      plugin        => 'webroot',
+      webroot_paths => ['/home/pyskata/www'],
+      manage_cron   => true,
+      require       => Nginx::Resource::Vhost['matuszewska.photo']
   }
-
-  include magda
 }
