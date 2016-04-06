6735de8e65efb2b31a96c1a7aec47edaeb61f0de
Łukasz Jan Niemier <lukasz@niemier.pl>
2016-04-06 20:04:31 +0200

    Fix typo

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,7 +12,7 @@ node default {
     group => root,
   }
 
-  class { 'cron-puppet': minutes => '*/10' }
+  class { 'cron-puppet': minute  => '*/10' }
   class { 'docker': docker_users => ['hauleth'] }
   class { 'nginx': manage_repo   => true, }
   class { 'letsencrypt': email   => 'lukasz@niemier.pl', }
